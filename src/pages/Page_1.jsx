51df928aef22e8807d2db5a8ce0d5f389fa9760f
tempiltin <temurbekshukurov0707@gmail.com>
2022-08-06 14:37:30 +0500

    upload

--- src/pages/Page_1.jsx
+++ src/pages/Page_1.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 
-import ReactPlayer from 'react-player'
-
 import video from "../video/Planet Earth Revolving.mp4"
+import '../lbs/video.css'
+
 const Page1 = () => {
 
-  return (
-    <ReactPlayer url={video} playing={true} />
+  const myhandlerEnded = (e)=>{
+    alert('video ended')
+  }
 
+
+  return (
+    <>
+      <video
+        id="my-video"
+        className="video-js"
+        controls
+        preload="auto"
+
+        poster="MY_VIDEO_POSTER.jpg"
+        data-setup="{}"
+        onEnded={myhandlerEnded}
+      >
+        <source src={video} type="video/mp4" />
+        <source src="MY_VIDEO.webm" type="video/webm" />
+      </video>
+
+      <script src="../lbs/vide.js"></script>
+    </>
   )
 }
 
