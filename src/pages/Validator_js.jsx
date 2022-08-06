88e430423f880241d08adbc00a33a27db10390ea
tempiltin <temurbekshukurov0707@gmail.com>
2022-08-06 17:16:41 +0500

    upload

--- src/pages/Validator_js.jsx
+++ src/pages/Validator_js.jsx
@@ -3,29 +3,30 @@ import AceEditor from "react-ace";
 
 
 import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-github";
+// import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-cobalt";
 import "ace-builds/src-noconflict/ext-language_tools"
 
 const Validatorjs = () => {
-  const  onChange = (newValue)=> {
+  const onChange = (newValue) => {
     console.log("change", newValue);
   }
-  
+
   return (
-<main className='main_editor'>
-<AceEditor 
-    mode="javascript"
-    theme="github"
-    onChange={onChange}
-    name="UNIQUE_ID_OF_DIV"
-    editorProps={{ $blockScrolling: true }}
-    setOptions={{
-      enableBasicAutocompletion: true,
-      enableLiveAutocompletion: true,
-      enableSnippets: true
-    }}
-  />
-</main>
+    <main className='main_editor'>
+      <AceEditor
+        mode="javascript"
+        theme="github"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true
+        }}
+      />
+    </main>
   )
 }
 
