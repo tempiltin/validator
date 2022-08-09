96443fd34286a65732cd45bc0401e12a135d8ba5
tempiltin <temurbekshukurov0707@gmail.com>
2022-08-09 15:19:29 +0500

    upload

--- src/pages/Java_Script_page.jsx
+++ src/pages/Java_Script_page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import AceEditor from "react-ace";
 
 
@@ -6,28 +6,75 @@ import "ace-builds/src-noconflict/mode-javascript";
 // import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/theme-cobalt";
 import "ace-builds/src-noconflict/ext-language_tools"
+import Form from '../Components/form';
+
 
 const Java_Script_page = () => {
+
+
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [code, setCode] = useState([])
+  const [codesubmit, setCodesubmit] = useState(false);
+  const [lang, setLang] = useState('');
+
   const onChange = (newValue) => {
     console.log("change", newValue);
+    setCode(newValue)
+    setLang('javascript')
+  }
+  const saveUser = () => {
+    console.log({ lastname, firstname, phone, code ,lang });
+
+    let data = { lastname, firstname, phone, code ,lang }
+    fetch("http://localhost:3000/python ", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((result) => {
+      console.warn("result", result);
+    })
+  }
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    if (codesubmit) {
+      codesubmit(false);
+    } else {
+      setCodesubmit(true);
+    }
   }
 
   return (
-    <main className='main_editor'>
-      <AceEditor
-        mode="javascript"
-        theme="github"
-        onChange={onChange}
-        name="UNIQUE_ID_OF_DIV"
-        editorProps={{ $blockScrolling: true }}
-        setOptions={{
-          enableBasicAutocompletion: true,
-          enableLiveAutocompletion: true,
-          enableSnippets: true
-        }}
+    <>
+      <Form saveUser={saveUser} setName={setFirstName}
+        setLastname={setLastname}
+        setPhone={setPhone}
+        firstname={firstname}
+        lastname={lastname}
+        phone={phone}
+        codesubmit={codesubmit}
       />
-    </main>
+      <main className='main_editor'>
+        <AceEditor
+          mode="javascript"
+          theme="github"
+          onChange={onChange}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true
+          }}
+        />
+        <button onClick={onSubmit} className="btn">Tekshirish</button>
+      </main>
+    </>
   )
 }
 
-export default Java_Script_page
+export default Java_Script_page;
